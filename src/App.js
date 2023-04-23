791bfdd94e60501a729adb2758d898643b86f9b4
Danush Athithya <danushathithya.24cs@licet.ac.in>
2023-04-23 13:20:30 +0530

    adds

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Awesome
+          Learn React Awesome 2
         </a>
       </header>
     </div>
